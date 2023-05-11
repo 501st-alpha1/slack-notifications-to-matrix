15e79e2ee4c4740e21f78c95e7c141757a10d8c6
Scott Weldon <open-source@scott-weldon.com>
2023-05-10 21:29:25 -0700

    Add method to check if user should be notified
    Calls API to see if this is a DM or channel the user has joined.

--- app.py
+++ app.py
@@ -7,6 +7,29 @@ app = App(
   signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
+def should_notify_channel(channel):
+  response = app.client.conversations_info(channel=channel)
+
+  if not response['ok']:
+    print('got an error in should_notify_channel')
+    print(response['error'])
+
+    # Notify user just in case.
+    return True
+
+  if response['channel']['is_im']:
+    # Always notify for IMs.
+    return True
+  elif response['channel']['is_mpim']:
+    # Always notify for group IMs.
+    return True
+  elif response['channel']['is_member']:
+    # Only joined channels otherwise.
+    return True
+  else:
+    print('Skipping notification for unjoined channel ' + response['channel']['name'])
+    return False
+
 @app.event("message")
 def handle_dm(client, event, logger):
   print('got an event')
